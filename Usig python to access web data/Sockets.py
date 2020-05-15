6d62f2cef3439ecb82249a2a4bd38e0bb1f726be
Seetaram_oruganti <orugantiseetaram@gmail.com>
2020-05-15 17:30:49 +0530

    Create Sockets.py

---
+++ Usig python to access web data/Sockets.py
@@ -0,0 +1,21 @@
+#   Retrive data from web page using sockets in python 
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
+
+
+
+
+
